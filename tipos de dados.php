7b90450731b4ea3dd487b18e0560f485996b6ce9
Thiago Silva <thiaggotx@gmail.com>
2023-01-27 23:53:07 -0300

    Atualizacao nova

---
+++ tipos de dados.php
@@ -0,0 +1,18 @@
+<?php
+
+//tipos básicos
+$nome = "HCode"; //string
+$site = 'www.hcode.com.br'; //string
+$ano = 1990; //int
+$salario = 5500.99; //ponto flutuante
+$bloqueado = false; //booleano
+
+//tipos compostos
+$frutas = array("abacaxi", "laranja", "manga"); //array
+echo $frutas[2];
+echo "</b>";
+
+$nascimento = new DateTime(); //objeto
+var_dump($nascimento); //mostra o tipo de dado
+
+?>
